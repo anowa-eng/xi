fea62373d3452a2983aeb7c52b4c6b2c94a66ee3
anowa-eng <60952646+anowa-eng@users.noreply.github.com>
2021-12-28 19:54:09 -0800

    fdsfrvtftsarvdf

--- xi.js
+++ xi.js
@@ -1,16 +1,20 @@
 var direction_variable = 0;
 var param1;
-class element {
-	constructor(elementToReturn) {
-		param1 = elementToReturn;
-		return document.querySelectorAll(param1);
-	}
-	static direction() {
-		transformgetter = '';
-		while (document.querySelectorAll(param1).transform.charAt(3))
-	}
-	static turn(turnBy) {
-		direction_variable += turnBy;
-		document.querySelectorAll(param1).style.transform = ('rotate(' + direction_variable + 'deg)'); 
+
+const xi = {
+	_XiElementConstructor: class {
+		constructor(elementToReturn) {
+			param1 = elementToReturn;
+			return document.querySelectorAll(param1);
+		}
+		static direction() {
+			transformgetter = '';
+			while (document.querySelectorAll(param1).transform.charAt(3))
+		}
+		static turn(turnBy) {
+			direction_variable += turnBy;
+			document.querySelectorAll(param1).style.transform = ('rotate(' + direction_variable + 'deg)'); 
+		}
 	}
 }
+
